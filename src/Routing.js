b74249c4ca18be3493d44afc05fdc0d29cc08817
earny4302 <73746161+earny4302@users.noreply.github.com>
2022-11-13 17:33:10 +0530

    Update Routing.js

--- src/Routing.js
+++ src/Routing.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route} from 'react-router-dom';
 import Recep from './component/recep';
 import Home from './component/Home';
 import servicesRecep from './component/servicesRecep';
+import AddPatient from './component/add_patient';
 
 const Routing = () => {
     return(
@@ -11,6 +12,7 @@ const Routing = () => {
                     <Route exact path="/" component={Home}/>
                     <Route exact path="/recep" component={Recep}/>
                     <Route exact path="/servicesRecep" component={servicesRecep}/>
+                    <Route exact path="/AddPatient" component={AddPatient}/>
                     
             </div>
         </BrowserRouter>
