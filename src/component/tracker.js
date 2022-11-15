790ef0dba6de978596c76dfe6aaca3809d113e15
earny4302 <73746161+earny4302@users.noreply.github.com>
2022-11-15 12:26:37 +0530

    Update tracker.js

--- src/component/tracker.js
+++ src/component/tracker.js
@@ -8,31 +8,45 @@ class Tracker extends Component {
     
     constructor(props){
         super(props)
-
         this.state={
-            pid:`gmh${Math.floor(Math.random()*10000)}`, 
-            name:'', 
-            age: '',
-            aadhaar: '',
-            phone: '',
-            gender: '', 
-            address: '',
-            emergency_name: '',
-            emergency_number:''
+            pid:`` 
+            
         }
+        
     }
-
-    
-
+    handleChange=(event) => {
+        this.setState({[event.target.name]:event.target.value})
+    }
     render(){
         return(
             <>
                 <div>
                     <Recep/>
                 </div>
-                <center>
-                <h1>Tracker Patient Here</h1>
-                </center>
+                <div id="headtrack">                    
+                    <h1>TRACK PATIENT</h1>
+                    
+                </div>
+
+                <div class="search-box">
+                        <input class="search-txt" onChange={this.handleChange} type="text" name="" placeholder="Type to search"/>
+                        <a class="search-btn" href="#">
+                            <i class="fas fa-search"></i>
+                        </a>
+                </div>
+
+                <div id="showpat"> 
+                    <img id="paticonbox" src="https://i.ibb.co/JCShHKQ/patient-1.png" ></img>
+
+                    <div id="patientinfobox">
+                        <p>ID:</p>
+                        <p>NAME:</p>
+                        <p>AGE:</p>
+                        <p>GENDER:</p>
+                        <p>ADDRESS:</p>
+                        <p>PHONE:</p>                        
+                    </div>
+                </div>
                 
             </>
         )
